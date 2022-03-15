dcf9a7f8d0a0004d5e4ee4db87999d8472509332
ReyAdolfo <adolfomontero3rey@gmail.com>
2022-03-15 11:26:20 +0800

    My Educational Background added

--- git-python-Rey.py.py
+++ git-python-Rey.py.py
@@ -1,3 +1,7 @@
 print ("Adolfo III Rey");
 print ("Brgy Uno, Poblacion, Buenavista, Marinduque");
 print ("September 13, 2002");
+print ("Marinduque State College");
+print ("BSIT");
+print ("Second Year");
+print ("Section B");
